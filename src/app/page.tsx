1d15cb0b751b023752341f635eba229242914465
Diego-Batista <diegobtistadev@gmail.com>
2025-05-26 16:05:47 -0300

    chore: add chadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="drak">
       <h1 className="gap-2 bg-amber-800 text-3xl font-bold text-amber-50 underline">
         Hello World
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
