98b1beaf5ed5143da272967fc935c255862a9dfb
Diego-Batista <diegobtistadev@gmail.com>
2025-05-27 18:00:39 -0300

    feat: add sign in form

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,15 +1,6 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
 
 export default function AuthenticationPage() {
@@ -21,15 +12,7 @@ export default function AuthenticationPage() {
           <TabsTrigger value="register">Criar conta</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>Faça login para continuar</CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Button>Entrar</Button>
-            </CardFooter>
-          </Card>
+          <SignInForm />
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm />
