b6b367001a7d834cbf6191f45df1a7e3001acb56
Diego-Batista <diegobtistadev@gmail.com>
2025-05-28 14:37:08 -0300

    feat: add clinic data to user session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins";
+import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import * as schema from "@/db/schema";
@@ -20,6 +22,29 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [
+    customSession(async ({ user, session }) => {
+      const clincs = await db.query.usersToClinicsTable.findMany({
+        where: eq(schema.usersToClinicsTable.userId, user.id),
+        with: {
+          clinic: true,
+        },
+      });
+      const clinic = clincs?.[0];
+      return {
+        session,
+        user: {
+          ...user,
+          clinic: clinic.clinicId
+            ? {
+                id: clinic.clinicId,
+                name: clinic?.clinic?.name,
+              }
+            : undefined,
+        },
+      };
+    }),
+  ],
   user: {
     modelName: "usersTable",
   },
